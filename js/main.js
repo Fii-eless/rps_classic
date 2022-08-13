2b31dd7ee14d708944c16839ebcc05cb8ba8f70c
fii-eless <fiifiad@gmail.com>
2022-08-13 10:13:59 +0400

    Define a variable that lets the player enter either rock paper or scissors

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 
+//create function that makes a random choice between rock, paper and scissors
+
 getComputerChoice = () => {
     const rps = ['rock' , 'paper' , 'scissors'];
     const computerChoice = rps[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+const getPlayerChoice = prompt("Please enter 'rock', 'paper', or ''scissors");
+const playerChoice = getPlayerChoice.toLowerCase();
+
+
