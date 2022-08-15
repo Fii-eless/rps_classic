9b7e62a21069be41bffa86663f7b9e6e2c98e5b3
fii-eless <fiifiad@gmail.com>
2022-08-15 11:18:37 +0400

    define a variable to contain getComputerChoice as a parameter for playGame function

--- js/main.js
+++ js/main.js
@@ -6,8 +6,25 @@ getComputerChoice = () => {
     const computerChoice = rps[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
+//console.log(getComputerChoice());
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
 
+//define a function that lets player enter rock, paper or scissors
 const getPlayerChoice = prompt("Please enter 'rock', 'paper', or ''scissors");
 const playerChoice = getPlayerChoice.toLowerCase();
+console.log(playerChoice);
 
+//define a function for gameplay logic using playerChoice vs computerChoice
+
+/*const gamePlay = (getComputerChoice() , playerChoice) => {
+    getComputerChoice() === playerChoice ? console.log('It is a tie game!') :
+    getComputerChoice() === 'rock' && playerChoice === 'paper' ? console.log(`You win! ${playerChoice} covers ${getComputerChoice()}`) :
+    getComputerChoice() === 'rock' && playerChoice === 'scissors' ? console.log(`Computer wins! ${getComputerChoice()} crushes ${playerChoice}`) :
+    getComputerChoice() === 'paper' && playerChoice === 'rock' ? console.log(`Computer wins! ${getComputerChoice()} covers ${playerChoice}`) :
+    getComputerChoice() === 'paper' && playerChoice === 'scissors' ? console.log(`You win! ${playerChoice} cuts ${getComputerChoice()}`) :
+    getComputerChoice() === 'scissors' && playerChoice === 'rock' ? console.log(`You win! ${playerChoice} crushes ${getComputerChoice()}`) :
+    console.log(`Computer wins! ${getComputerChoice()} cuts ${playerChoice}`)
+}
+gamePlay();*/
 
