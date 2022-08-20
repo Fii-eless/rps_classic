9c068b70df301fa6ebc6927614c2c95da306ef26
fii-eless <fiifiad@gmail.com>
2022-08-20 09:31:00 +0400

    Write gamePlay logic and get all functions working

--- js/main.js
+++ js/main.js
@@ -1,44 +1,81 @@
 //define a function to initialize game
-gameStart = () => {
+const gameStart = () => {
     let startGame = confirm('Shall we play a game of Rock-Paper-Scissors?');
+    startGame ? playGame() : alert("You're not in the mood, maybe later!");
+}
 
-    startGame ? getPlayerChoice() : endGame() ;
-    getPlayerChoice === 'rock' || getPlayerChoice === 'paper' || getPlayerChoice === 'scissors' ? gamePlay() : didNotChoose();
-    
+//define playGame function with loops
+const playGame = () => {
+    while (true) {
+        let playerChoice = getPlayerChoice();
+        playerChoice = formatPlayerChoice(playerChoice);
+        if (playerChoice === "") {
+            didNotChoose();
+            continue;
+        }
+        if (!playerChoice) {
+            endGame();
+            break;
+        }
+        playerChoice = evaluatePlayerChoice(playerChoice);
+        if (!playerChoice) {
+            didNotChoose();
+            continue;
+        }
+        const computerChoice = getComputerChoice();
+        const result = gamePlay(playerChoice , computerChoice);
+
+        displayResult = (result) => {
+            alert(result);
+        }
+    }
+
 }
 
 //create function that makes a random choice between rock, paper and scissors
 
-getComputerChoice = () => {
-    const rps = ['rock' , 'paper' , 'scissors'];
-    const computerChoice = rps[Math.floor(Math.random() * 3)];
-    return computerChoice;
+const getPlayerChoice = () => {
+    prompt("Please enter 'rock', 'paper' or 'scissors'");
 }
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
 
 //define a function that lets player enter rock, paper or scissors
-getPlayerChoice = () => {
-    const choose = prompt("Please enter 'rock', 'paper', or ''scissors");
-const playerChoice = choose.trim().toLowerCase();
-return playerChoice;
+const formatPlayerChoice = (playerChoice) => {
+   if (playerChoice || playerChoice === "") {
+    return playerChoice.trim().toLowerCase() ;
+   } else {
+    return false;
+   }
 }
 
+const evaluatePlayerChoice = (playerChoice) => {
+    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
+        return playerChoice;
+    } else {
+        false;
+    }
+}
+const getComputerChoice = () => {
+    const rps = ['rock' , 'paper' , 'scissors'];
+    const Choice = rps[Math.floor(Math.random() * 3)];
+    return Choice;
+}
 
 //define a function for gameplay logic using playerChoice vs computerChoice
 
- gamePlay = (computerChoice , playerChoice) => {
-    computerChoice === playerChoice ? console.log('It is a tie game!') :
-    computerChoice === 'rock' && playerChoice === 'paper' ? console.log(`You win! ${playerChoice} covers ${computerChoice}`) :
-    computerChoice === 'rock' && playerChoice === 'scissors' ? console.log(`Computer wins! ${computerChoice} crushes ${playerChoice}`) :
-    computerChoice === 'paper' && playerChoice === 'rock' ? console.log(`Computer wins! ${computerChoice} covers ${playerChoice}`) :
-    computerChoice === 'paper' && playerChoice === 'scissors' ? console.log(`You win! ${playerChoice} cuts ${computerChoice}`) :
-    gcomputerChoice === 'scissors' && playerChoice === 'rock' ? console.log(`You win! ${playerChoice} crushes ${computerChoice}`) :
-    console.log(`Computer wins! ${computerChoice} cuts ${playerChoice}`)
+ const gamePlay = (computerChoice , playerChoice) => {
+    const winner = computerChoice === playerChoice ? 'It is a tie game!' :
+    computerChoice === 'rock' && playerChoice === 'paper' ? `You win! ${playerChoice} covers ${computerChoice}` :
+    computerChoice === 'rock' && playerChoice === 'scissors' ? `Computer wins! ${computerChoice} crushes ${playerChoice}` :
+    computerChoice === 'paper' && playerChoice === 'rock' ? `Computer wins! ${computerChoice} covers ${playerChoice}` :
+    computerChoice === 'paper' && playerChoice === 'scissors' ? `You win! ${playerChoice} cuts ${computerChoice}` :
+    computerChoice === 'scissors' && playerChoice === 'rock' ? `You win! ${playerChoice} crushes ${computerChoice}` :
+    `Computer wins! ${computerChoice} cuts ${playerChoice}`;
+
+    return winner;
 }
-gamePlay();
 
-endGame = () => alert("You're not in the mood, maybe later!");
+const didNotChoose = () => alert("You did not enter 'rock', 'paper' or 'scissors'. Let's try again!");
 
-didNotChoose = () => alert("You did not enter 'rock', 'papper' or 'scissors'. Let's try again!")
+const endGame = () => alert("I guess you changed your mind, maybe next time!");
 
+gameStart();
