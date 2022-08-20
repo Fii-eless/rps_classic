9ad5fd4df5928cf62f1badd906437e1432ceba3a
fii-eless <fiifiad@gmail.com>
2022-08-20 22:39:21 +0400

    Complete playGame function and add determineWinner

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //define a function to initialize game
 const gameStart = () => {
-    let startGame = confirm('Shall we play a game of Rock-Paper-Scissors?');
+    const startGame = confirm('Shall we play a game of Rock-Paper-Scissors?');
     startGame ? playGame() : alert("You're not in the mood, maybe later!");
-}
+};
 
 //define playGame function with loops
 const playGame = () => {
@@ -12,31 +12,27 @@ const playGame = () => {
         if (playerChoice === "") {
             didNotChoose();
             continue;
-        }
+        } 
         if (!playerChoice) {
             endGame();
             break;
         }
-        playerChoice = evaluatePlayerChoice(playerChoice);
+         playerChoice = evaluatePlayerChoice(playerChoice);
         if (!playerChoice) {
             didNotChoose();
             continue;
         }
         const computerChoice = getComputerChoice();
-        const result = gamePlay(playerChoice , computerChoice);
-
-        displayResult = (result) => {
-            alert(result);
-        }
+        const result = gamePlay(computerChoice , playerChoice);
+        displayResult(result);
     }
-
-}
+};
 
 //create function that makes a random choice between rock, paper and scissors
 
 const getPlayerChoice = () => {
-    prompt("Please enter 'rock', 'paper' or 'scissors'");
-}
+    return prompt("Please enter 'rock', 'paper' or 'scissors'");
+};
 
 //define a function that lets player enter rock, paper or scissors
 const formatPlayerChoice = (playerChoice) => {
@@ -45,7 +41,9 @@ const formatPlayerChoice = (playerChoice) => {
    } else {
     return false;
    }
-}
+};
+
+const endGame = () => alert("I guess you changed your mind, maybe next time!");
 
 const evaluatePlayerChoice = (playerChoice) => {
     if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
@@ -54,6 +52,8 @@ const evaluatePlayerChoice = (playerChoice) => {
         false;
     }
 }
+const didNotChoose = () => alert("You did not enter 'rock', 'paper' or 'scissors'. Let's try again!");
+
 const getComputerChoice = () => {
     const rps = ['rock' , 'paper' , 'scissors'];
     const Choice = rps[Math.floor(Math.random() * 3)];
@@ -62,20 +62,18 @@ const getComputerChoice = () => {
 
 //define a function for gameplay logic using playerChoice vs computerChoice
 
- const gamePlay = (computerChoice , playerChoice) => {
-    const winner = computerChoice === playerChoice ? 'It is a tie game!' :
-    computerChoice === 'rock' && playerChoice === 'paper' ? `You win! ${playerChoice} covers ${computerChoice}` :
-    computerChoice === 'rock' && playerChoice === 'scissors' ? `Computer wins! ${computerChoice} crushes ${playerChoice}` :
-    computerChoice === 'paper' && playerChoice === 'rock' ? `Computer wins! ${computerChoice} covers ${playerChoice}` :
-    computerChoice === 'paper' && playerChoice === 'scissors' ? `You win! ${playerChoice} cuts ${computerChoice}` :
-    computerChoice === 'scissors' && playerChoice === 'rock' ? `You win! ${playerChoice} crushes ${computerChoice}` :
-    `Computer wins! ${computerChoice} cuts ${playerChoice}`;
+ const gamePlay = (computer , player) => {
+    const winner = computer === player ? 'It is a tie game!' :
+    computer === 'rock' && player === 'paper' ? `You win! ${player} covers ${computer}` :
+    computer === 'rock' && player === 'scissors' ? `Computer wins! ${computer} crushes ${player}` :
+    computer === 'paper' && player === 'rock' ? `Computer wins! ${computer} covers ${player}` :
+    computer === 'paper' && player === 'scissors' ? `You win! ${player} cuts ${computer}` :
+    computer === 'scissors' && player === 'rock' ? `You win! ${player} crushes ${computer}` :
+    `Computer wins! ${computer} cuts ${player}`;
 
     return winner;
 }
-
-const didNotChoose = () => alert("You did not enter 'rock', 'paper' or 'scissors'. Let's try again!");
-
-const endGame = () => alert("I guess you changed your mind, maybe next time!");
+const displayResult = (result) => {
+    console.log(result);}
 
 gameStart();
