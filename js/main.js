ac2ca59b4fed7be20be3884c82b5173f321265ab
fii-eless <fiifiad@gmail.com>
2022-11-14 22:46:22 +0400

    Create function that plays game when rock is clicked and another that returns score when game is played

--- js/main.js
+++ js/main.js
@@ -1,65 +1,12 @@
-//define a function to initialize game
-const gameStart = () => {
-    const startGame = confirm('Shall we play a game of Rock-Paper-Scissors?');
-    startGame ? playRounds() : alert("You're not in the mood, maybe later!");
-};
-
-//define playGame function with loops
-const playGame = () => {
-    while (true) {
-        let playerChoice = getPlayerChoice();
-        playerChoice = formatPlayerChoice(playerChoice);
-        if (playerChoice === "") {
-            didNotChoose();
-            continue;
-        } 
-        if (!playerChoice) {
-            endGame();
-            break;
-        }
-         playerChoice = evaluatePlayerChoice(playerChoice);
-        if (!playerChoice) {
-            didNotChoose();
-            continue;
-        }
-        const computerChoice = getComputerChoice();
-        const result = gamePlay(computerChoice , playerChoice);
-        displayResult(result);
-        return result;
-    };
-};
-
-//create function that makes a random choice between rock, paper and scissors
-
-const getPlayerChoice = () => {
-    return prompt("Please enter 'rock', 'paper' or 'scissors'");
-};
-
-//define a function that lets player enter rock, paper or scissors
-const formatPlayerChoice = (playerChoice) => {
-   if (playerChoice || playerChoice === "") {
-    return playerChoice.trim().toLowerCase() ;
-   } else {
-    return false;
-   }
-};
-
-const endGame = () => alert("I guess you changed your mind, maybe next time!");
-
-const evaluatePlayerChoice = (playerChoice) => {
-    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-        return playerChoice;
-    } else {
-        false;
-    }
-}
-const didNotChoose = () => alert("You did not enter 'rock', 'paper' or 'scissors'. Let's try again!");
+let round = 0;
+let compWins = 0;
+let youWin = 0;
 
 const getComputerChoice = () => {
     const rps = ['rock' , 'paper' , 'scissors'];
     const Choice = rps[Math.floor(Math.random() * 3)];
     return Choice;
-}
+};
 
 //define a function for gameplay logic using playerChoice vs computerChoice
 
@@ -74,60 +21,105 @@ const getComputerChoice = () => {
 
     return winner;
 }
-const displayResult = (result) => {
-    console.log(result);}
 
-const computerTriumph = (playOn) => {
-    const compWins = playOn.includes("Computer wins");
-    return compWins;
+const computerTriumph = (gamePlay) => {
+    const computerWins = gamePlay.includes("Computer wins");
+    return computerWins;
 }
-const playerTriumph = (playOn) => {
-    const pWins = playOn.includes("You win");
+const playerTriumph = (gamePlay) => {
+    const pWins = gamePlay.includes("You win");
     return pWins;
 }
+const playAgain = () => {
+    return " Play Again!"
+};
+
+const endRounds = () => {
+    return " Your Quest Ends Here!";
+}
  
-// Function calls playGame five times and returns score after each round
-const playRounds = () => {
-    let round = 1
-    let computerScore = 0;
-    let playerScore = 0;
-   
-    while (round <= 5) {
-        console.log(`Round ${round}`);
-        // playGame();
-        const playOn = playGame();
-        const computerWins = computerTriumph(playOn);
-        const playerWins = playerTriumph(playOn);
-        
-        if (computerWins) {
-            computerScore++;
-            console.log(`Computer(${computerScore}) : Player(${playerScore})`)
-        } else if (playerWins){
-            playerScore++;
-            console.log(`Computer(${computerScore}) : Player(${playerScore})`)
-        }else {
-            computerScore++;
-            playerScore++;
-            console.log(`Computer(${computerScore}) : Player(${playerScore})`)
-        }
-        if (round === 5) {
-            const callWinner = declareChamp(computerScore , playerScore);
-            return callWinner;
-        }
-
-        round++;
-    }
-}
-
 //After 5 rounds of play, function names a champion
-const declareChamp = (computerScore , playerScore) => {
-    if (computerScore === playerScore) {
-        console.log(`\nComputer(${computerScore}) : Player(${playerScore})\nIt is a tie!`);
-    } else if (computerScore > playerScore) {
-        console.log(`\nComputer(${computerScore}) : Player(${playerScore})\nComputer is Champion!`);
+function declareChamp (computerScore , playerScore) {
+    if (computerScore > playerScore) {
+        return "Computer won! You know what to do!";
     } else {
-        console.log(`\nComputer(${computerScore}) : Player(${playerScore})\nYou are Champion!`);
+        return "You won! You know what to do!";
     }
 }
 
-gameStart();
+function playNow(playerChoice) {
+    let computerChoice = getComputerChoice();
+    if (playerChoice && computerChoice) {
+        let playOutcome = gamePlay(computerChoice, playerChoice);
+        return playOutcome;
+    };  
+};
+
+function getScore(playOutcome) {
+    let playerWin = playerTriumph(playOutcome);
+    let computerWin = computerTriumph(playOutcome);
+  
+    if (round > 4) {
+        return
+    }
+    if(playerWin || computerWin && round < 5) {
+        ++round;
+        console.log(round);
+    };
+    if (playerWin) {
+        youWin += 1;
+    };
+    if (computerWin) {
+        compWins += 1;
+    };
+    rounds.textContent = round;
+
+    const again = playAgain();
+    const endGame = endRounds();
+
+    if (round < 5) {
+        showResult.textContent = playOutcome + again
+    } else if (round == 5) {
+        showResult.textContent = playOutcome + endGame;
+    } else if (round < 5) {
+        return
+    };
+    playerScore.textContent = youWin;
+    computerScore.textContent = compWins;
+
+    if (round == 5) {
+        let callWinner = declareChamp(compWins, youWin);
+        declaration.textContent = callWinner;
+    };
+    } 
+
+const choice = document.querySelector('.choices');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+// let playerChoice = "";
+let showResult = document.querySelector('#showResult');
+let rounds = document.querySelector('#rounds');
+let playerScore = document.querySelector('#playerScore');
+let computerScore = document.querySelector('#computerScore');
+let declaration = document.querySelector('#declaration');
+
+const chooseRock = (e) => {
+    let playerChoice = "";
+    playerChoice += "rock";
+    let result = playNow(playerChoice);
+    getScore(result);
+ };
+ const choosePaper = (e) => {
+    playerChoice += "paper";
+    getComputerChoice();
+ };
+ const chooseScissors = (e) => {
+    playerChoice += "scissors";
+    getComputerChoice();
+ };
+
+rock.addEventListener('click', chooseRock);
+paper.addEventListener('click', choosePaper);
+scissors.addEventListener('click', chooseScissors);
+
